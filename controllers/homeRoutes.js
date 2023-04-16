91e04736a619eaadbc101fd49bbdc58037835284
mnyzio <nyziu84@gmail.com>
2023-04-15 21:54:34 -0400

    basic controller set up

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,21 @@
 const router = require('express').Router();
-
+const { Post, User, Comment } = require("../models");
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    try {
-        res.render('homepage');
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    res.render('homepage',
+      {
+        logged_in: req.session.logged_id,
+        user_id: req.session.user_id,
+        user_name: req.session.user_name
+      }
+    );
+  } catch (err) {
+    res.status(500).json(err);
+  }
 })
+
+
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
-
 const homeRoutes = require('./homeRoutes');
+const postRoutes = require("./posts");
+const userRoutes = require("./userRoutes");
+
 
 router.use('/', homeRoutes);
+router.use('/posts', postRoutes);
+router.use('/', userRoutes);
+
+// Fallback route for when a user attempts to visit routes that don't exist
+router.get('*', (req, res) => {
+    res.render('unknown', {
+        logged_in: req.session.logged_in,
+        user_id: req.session.user_id,
+        user_name: req.session.user_name
+    },
+    )
+});
 
 module.exports = router;
