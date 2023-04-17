c070f49c5ffdeb3171991cefd8fa31b33354c99d
mnyzio <nyziu84@gmail.com>
2023-04-17 01:02:54 -0400

    comment functionality, updated styling

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const postRoutes = require("./posts/postRoutes");
 const userRoutes = require("./userRoutes");
+const commentRoutes = require("./comments/commentRoutes");
 
 
 router.use('/', homeRoutes);
 router.use('/', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 // Fallback route for when a user attempts to visit routes that don't exist
 router.get('*', (req, res) => {
---
+++ public/js/add-comment.js
@@ -0,0 +1,39 @@
+const commentSubmitBtn = document.getElementById('comment-submit');
+const commentBody = document.getElementById('comment_body');
+const postID = document.getElementById('post-id').value;
+
+// Enable comment submit button if comment input field is not empty
+commentBody.addEventListener('keyup', () => {    
+    if (commentBody.value == "") {        
+        commentSubmitBtn.setAttribute("class", "pointer-events-none p-4 text-xl border bg-slate-200 border-emerald-400 hover:bg-emerald-400 transition duration-300")
+    } else {
+        commentSubmitBtn.setAttribute("class", "text-xl p-4 shadow-lg border border-emerald-400 hover:bg-emerald-300 active:shadow-sm transition duration-300");
+    }
+});
+
+// Add comment to post
+commentSubmitBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    try {
+        const response = await fetch("/comments", {
+            method: "POST",
+            body: JSON.stringify({
+                comment_body: commentBody.value,                
+                post_id: postID,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        };
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
