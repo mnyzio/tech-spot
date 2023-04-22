33f9b275f4d0b8a4d7071729e1e287d509a6829b
mnyzio <nyziu84@gmail.com>
2023-04-22 02:10:06 -0400

    disable seach if emplty input field

--- public/js/search.js
+++ public/js/search.js
@@ -4,15 +4,18 @@ const searchForm = document.getElementById('search-form');
 
 searchForm.addEventListener('submit' , (e) => {
     e.preventDefault();
-    e.preventDefault();
-    const searchValue = searchPosts.value;
-    document.location.replace(`/search/${searchValue}`)    
+    if (searchPosts.value !== "") {
+        const searchValue = searchPosts.value;
+        document.location.replace(`/search/${searchValue}`)    
+    }
 });
 
 searchBtn.addEventListener('click', async (e) => {
     e.preventDefault();
-    const searchValue = searchPosts.value;
-    document.location.replace(`/search/${searchValue}`)    
+    if (searchPosts.value !== "") {
+        const searchValue = searchPosts.value;
+        document.location.replace(`/search/${searchValue}`)    
+    }
 });
 
 
