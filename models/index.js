cb38bdd69dda4366b768638a22899fc2d410790f
mnyzio <nyziu84@gmail.com>
2023-04-22 14:42:04 -0400

    corrected model index and seed files

--- models/index.js
+++ models/index.js
@@ -32,18 +32,4 @@ Comment.belongsTo(Post, {
   foreignKey: "post_id",
 });
 
-Post.belongsToMany(User, {
-  through: {
-    model: Views,
-  },
-  foreignKey: "post_id",
-});
-
-User.belongsToMany(Post, {
-  through: {
-    model: Views,
-  },
-  foreignKey: "user_id",
-});
-
 module.exports = { User, Post, Comment, Views };
